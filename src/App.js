7921cf5aaedc0c67461735c1dec238321493699a
India Rodriguez <carnes.s.ia17@gmail.com>
2019-10-05 18:06:16 -0600

    worked through sections 8-10.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import Split from './composition/Split';
+import React, { Component } from 'react';
+//import Split from './composition/Split';
 import './App.css';
-import Tooltip from './composition/Tooltip';
+//import Tooltip from './composition/Tooltip';
+import TheDate from './state/TheDate';
+import Counter from './state/Counter'
+import Messages from './Messages';
+import Tabs from './state/Tabs';
+import Bomb from './state-drills/Bomb';
+import HelloWorld from './state-drills/HelloWorld';
+import RouletteGun from './state-drills/RouletteGun';
+import Accordion from './state-drills/Accordion';
 
-//make 2 tooltips here and another inside the App directly
+/* make 2 tooltips here and another inside the App directly
 const firstTooltip = (
   <Tooltip color='hotpink' message = 'tooltip message'>
     ipsum
@@ -18,6 +26,10 @@ const secondTooltip = (
 function App() {
   return (
     <main className='App'>
+      <TheDate>
+        <div></div>
+      </TheDate>
+      <Counter  count={123} />
       <Split className='left' flexBasis={2}>
         This is the content for the left `Split`. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
         <Tooltip message='one more tooltip message'>
@@ -29,7 +41,76 @@ function App() {
       </Split>
     </main>
   );
+} */
+
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
+
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
+
+class App extends Component {
+  render() {
+    return (
+      <main className="App">
+        <nav className="nav-bar">
+        <h1>REACT PLAYGROUND!</h1>
+        <div className="Date">
+        <TheDate>
+        <div></div>
+        </TheDate>
+        </div>
+        <div className='message'>
+        <Messages name="Messages" unread={0}/>
+        <Messages name="Notifications" unread={10}/>
+        </div>
+        <div className='Bomb'>
+          <h3>Time's running out...</h3>
+          <Bomb />
+        </div>
+        </nav>
+        <div className='Tabs'>
+          <Tabs tabs={tabsProp} />
+        </div>
+        <div className='Counter'>
+          <h3>How high can the counter go?</h3>
+        <Counter  count={123} />
+        </div>
+        <div className='HelloWorld'>
+          <h3>Say hello...</h3>
+          <HelloWorld />
+        </div>
+        <div className='Gun'>
+          <h4>Russian Roulette</h4>
+          <RouletteGun bulletInChamber={6} />
+        </div>
+        <div>
+          <h3>Accordion</h3>
+          <Accordion sections={sections}/>
+        </div>
+      </main>
+    );
+  }
 }
+
 
 export default App;
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
 import './index.css';
 //import HelloWorld from './state-drills/HelloWorld';
 //import Bomb from './state-drills/Bomb';
 import RouletteGun from './state-drills/RouletteGun';
+import App from './App';
 
-ReactDOM.render(<RouletteGun />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
